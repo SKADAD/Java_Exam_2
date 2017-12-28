c239e29bbc73ccef770634c431693b5f71d85b15
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-28 14:06:46 +0100

    Added Courses w/ getters/setters

--- Java_Exam_2/src/Server/Model/Courses.java
+++ Java_Exam_2/src/Server/Model/Courses.java
@@ -1,4 +1,41 @@
 package Server.Model;
 
+
 public class Courses {
+
+    private int courseID;
+    private String courseName;
+    private String courseTeacher;
+
+    public Course(int courseID, String courseName, String courseTeacher){
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.courseTeacher = courseTeacher;
+
+
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public java.lang.String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(java.lang.String courseName) {
+        this.courseName = courseName;
+    }
+
+    public java.lang.String getCourseTeacher() {
+        return courseTeacher;
+    }
+
+    public void setCourseTeacher(java.lang.String courseTeacher) {
+        this.courseTeacher = courseTeacher;
+    }
 }
