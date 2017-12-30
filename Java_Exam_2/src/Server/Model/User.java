4e877d62391511a647ab708ee786c037a2c03f46
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-30 20:42:10 +0100

    Removed teacher and remade User to include Teacher and Admin users for ease of use in DB

--- Java_Exam_2/src/Server/Model/User.java
+++ Java_Exam_2/src/Server/Model/User.java
@@ -1,16 +1,38 @@
 package Server.Model;
 
+import java.util.ArrayList;
+
 public class User {
+    private int userId;
     private String name;
     private String username;
     private String password;
+    private String email;
+    private ArrayList<String> courses;
+    private Boolean isTeacher;
+    private Boolean isAdmin;
 
 
-    public User(String name, String username, String password){
+
+    public User(int userId,String name, String username, String password, String email, ArrayList<String> courses){
+        this.userId = userId;
         this.name = name;
         this. username = username;
         this.password = password;
+        this.email = email;
+        this.courses = courses;
+        this.isTeacher = false;
+        this.isAdmin = false;
 
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
     public String getName() {
@@ -35,6 +57,38 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<String> courses) {
+        this.courses = courses;
+    }
+
+    public Boolean getTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(Boolean teacher) {
+        isTeacher = teacher;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
     }
 }
 
